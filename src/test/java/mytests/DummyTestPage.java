b162f52035c08b78801c369fee72545fad463d96
barikpooja5 <barikpooja5@gmail.com>
2024-10-02 10:03:48 +0530

    Create DummyTestPage.java

---
+++ src/test/java/mytests/DummyTestPage.java
@@ -0,0 +1,13 @@
+package mytests;
+
+import org.openqa.selenium.By;
+
+public class DummyTestPage {
+	
+	By userName = By.id("username");
+	
+	public void getDummyTestPage() {
+		System.out.println("DummyTestPage");
+	}
+
+}
